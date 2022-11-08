099cf210f6a644c90ad158b9ee58d9ba4a4e58ff
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-08 14:43:06 +0530

    Maximum floating number for second position

--- GenericsPractice/GenericsPractice/Program.cs
+++ GenericsPractice/GenericsPractice/Program.cs
@@ -7,8 +7,8 @@
             Console.WriteLine("\n***************************** WELCOME TO GENERICS PRACTICE PROBLEM ******************************");
 
 
-            double maxFlotingNo = GenericsMax.MaxIntNumber(85.4, 17.6, 19.0);
-            Console.WriteLine("\n Maximum floating number for 1st position is = " + maxFlotingNo);
+            double maxFlotingNo = GenericsMax.MaxIntNumber(85.4, 117.6, 19.0);
+            Console.WriteLine("\n Maximum floating number for 2nd position is = " + maxFlotingNo);
         }
     }
 }
