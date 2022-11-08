570fae549ad8d67692b0d7333c5ffed3caaf202f
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-08 14:23:48 +0530

    Maximum number at 2nd position

--- GenericsPractice/GenericsPractice/GenericsMax.cs
+++ GenericsPractice/GenericsPractice/GenericsMax.cs
@@ -24,7 +24,7 @@ namespace GenericsPractice
             {
                 return thirdNum;
             }
-            return firstNum;
+            return secondNum;
         }
     }
 }
--- GenericsPractice/GenericsPractice/Program.cs
+++ GenericsPractice/GenericsPractice/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("\n***************************** WELCOME TO GENERICS PRACTICE PROBLEM ******************************");
 
 
-            int maxNumber = GenericsMax.MaxIntNumber(854, 764, 909);
+            int maxNumber = GenericsMax.MaxIntNumber(854, 1764, 909);
             Console.WriteLine("\n Maximum Number is " + maxNumber);
         }
     }
