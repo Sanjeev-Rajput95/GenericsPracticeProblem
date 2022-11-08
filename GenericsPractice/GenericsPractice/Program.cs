5b66ca2ab6dec14eb76fafa8efc17db88e1e3ded
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-08 15:12:56 +0530

    Max String At 3rd Position

--- GenericsPractice/GenericsPractice/Program.cs
+++ GenericsPractice/GenericsPractice/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("\n***************************** WELCOME TO GENERICS PRACTICE PROBLEM ******************************");
 
 
-            string maxStr = GenericsMax.MaxIntNumber("Apple", "Peach", "Banana");
+            string maxStr = GenericsMax.MaxIntNumber("Apple", "Banana", "Peach");
             Console.WriteLine("\n Maximum string is : " + maxStr);
         }
     }
