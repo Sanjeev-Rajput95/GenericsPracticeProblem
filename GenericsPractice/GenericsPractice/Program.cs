d0fb9969da4fe13020ea562022b292e91fb28993
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-08 14:14:06 +0530

    Maximum Integer Number

---
+++ GenericsPractice/GenericsPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericsPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n***************************** WELCOME TO GENERICS PRACTICE PROBLEM ******************************");
+
+
+            int maxNumber = GenericsMax.MaxIntNumber(854, 764, 909);
+            Console.WriteLine("\n Maximum Number is " + maxNumber);
+        }
+    }
+}
