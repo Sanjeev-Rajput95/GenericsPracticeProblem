1d66cda127cc68384c8bbfcc762071fa006a6c7d
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-09 23:18:49 +0530

    Refactor : Generic Class

--- GenericsPractice/GenericsPractice/GenericsMax.cs
+++ GenericsPractice/GenericsPractice/GenericsMax.cs
@@ -6,26 +6,34 @@ using System.Threading.Tasks;
 
 namespace GenericsPractice
 {
-    public class GenericsMax
+    public class GenericsMax<T>where T : IComparable
     {
+            public T firstNum, secondNum, thirdNum;
 
-        public static T MaxIntNumber<T> (T firstNum, T secondNum, T thirdNum) where T : IComparable
-        {
-            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0 || firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0 || firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0 )
+            public GenericsMax(T firstNum, T secondNum, T thirdNum)
             {
+                this.firstNum = firstNum;
+                this.secondNum = secondNum;
+                this.thirdNum = thirdNum;
+            }
+
+            public static T MaxIntegerNumber(T firstNum, T secondNum, T thirdNum)
+            {
+                if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0 || firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) > 0 || firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) >= 0)
+                {
+                    return firstNum;
+                }
+                if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0 || secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0 || secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0)
+                {
+                    return secondNum;
+                }
+
+                if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(firstNum) > 0 || thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(firstNum) > 0 || thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(firstNum) >= 0)
+                {
+                    return thirdNum;
+                }
                 return firstNum;
             }
-
-            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0 || secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) > 0 || secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) >= 0)
-            {
-                return secondNum;
-            }
-
-            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0 || thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(secondNum) > 0 || thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) >= 0)
-            {
-                return thirdNum;
-            }
-            return firstNum;
-        }
+        
     }
 }
--- GenericsPractice/GenericsPractice/Program.cs
+++ GenericsPractice/GenericsPractice/Program.cs
@@ -6,11 +6,11 @@
         {
             Console.WriteLine("\n***************************** WELCOME TO GENERICS PRACTICE PROBLEM ******************************");
 
-          //  GenericsMax genericsObj = new GenericsMax();
+        
 
-            Console.WriteLine("Maximum Interger Number is : " + GenericsMax.MaxIntNumber<int>(30, 20, 10));
-            Console.WriteLine("Maximum Float Number is : " + GenericsMax.MaxIntNumber<float>(30.3f, 45.20f, 76.10f));
-            Console.WriteLine("Maximum String Number is : " + GenericsMax.MaxIntNumber<string>("78", "34", "10"));
+            Console.WriteLine("Maximum Interger Number is : " + GenericsMax<int>.MaxIntegerNumber(30, 20, 10));
+            Console.WriteLine("Maximum Float Number is : " + GenericsMax<float>.MaxIntegerNumber(30.3f, 45.20f, 76.10f));
+            Console.WriteLine("Maximum String Number is : " + GenericsMax<string>.MaxIntegerNumber("78", "34", "10"));
 
         }
     }
